950f8e0be81d99f6b7436f3804a390fc8ede0cdf
Matthew Hamilton <matthew.hamilton@orygen.org.au>
2021-11-17 15:51:35 +1100

    revirtualised classes

--- R/C4_Ready4Public.R
+++ R/C4_Ready4Public.R
@@ -2,13 +2,13 @@
 #'
 #' A virtual class denoting a module of the ready4 representational system hat is suitable for public dissemination in its current form.
 #'
+#' @slot dissemination_1L_chr Dissemination (a character vector of length one)
 #' @include C4_Ready4Module.R
 #' @name Ready4Public-class
 #' @rdname Ready4Public-class
 #' @export Ready4Public
 #' @exportClass Ready4Public
 Ready4Public <- setClass("Ready4Public",
-                         contains = "Ready4Module"#,
-                         # slots = c(dissemination_1L_chr = "character"),
-                         # prototype = list(dissemination_1L_chr = "Public: All of the data contained in this object is appropriate for public dissemination. This object can be shared publicly in its current form, under licensing terms defined by the data custodian.")
-                         )
+                         contains = "Ready4Module",
+                         slots = c(dissemination_1L_chr = "character"),
+                         prototype = list(dissemination_1L_chr = "Public: All of the data contained in this object is appropriate for public dissemination. This object can be shared publicly in its current form, under licensing terms defined by the data custodian."))
