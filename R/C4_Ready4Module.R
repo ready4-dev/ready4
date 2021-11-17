e0ebcf720b9bfd4e3b8364bf4c2075f3c806d139
Matthew Hamilton <matthew.hamilton@orygen.org.au>
2021-11-17 15:24:02 +1100

    revirtualised classes

--- R/C4_Ready4Module.R
+++ R/C4_Ready4Module.R
@@ -1,14 +1,13 @@
 #' Ready4Module
 #'
-#' A module, the foundational component of the ready4 representational system.
+#' A virtual class denoting a module of the ready4 representational system.
 #'
-#' @slot dissemination_1L_chr Dissemination (a character vector of length one)
 #' @name Ready4Module-class
 #' @rdname Ready4Module-class
 #' @export Ready4Module
 #' @exportClass Ready4Module
-Ready4Module <- setClass("Ready4Module",
-                         slots = c(dissemination_1L_chr = "character"),
-                         prototype = list(dissemination_1L_chr = "Undefined: Some or all of the data contained in this object may be appropriate for public dissemination, but this has yet to be confirmed or detailed by the relevant data custodians."))
-
-
+Ready4Module <- setClass("Ready4Module"#,
+                         # slots = c(dissemination_1L_chr = "character"),
+                         # prototype = list(dissemination_1L_chr = "Undefined: Some or all of the data contained in this object may be appropriate for public dissemination, but this has yet to be confirmed or detailed by the relevant data custodians.")
+                         )
+# #' @slot dissemination_1L_chr Dissemination (a character vector of length one)
--- R/C4_Ready4Private.R
+++ R/C4_Ready4Private.R
@@ -1,14 +1,14 @@
 #' Ready4Private
 #'
-#' A module of the ready4 representational system that contains data not intended for public dissemination.
+#' A virtual class denoting a module of the ready4 representational system that contains data not intended for public dissemination.
 #'
 #' @include C4_Ready4Module.R
-#' @slot dissemination_1L_chr Dissemination (a character vector of length one)
 #' @name Ready4Private-class
 #' @rdname Ready4Private-class
 #' @export Ready4Private
 #' @exportClass Ready4Private
 Ready4Private <- setClass("Ready4Private",
-                          contains = "Ready4Module",
-                          slots = c(dissemination_1L_chr = "character"),
-                          prototype = list(dissemination_1L_chr = "Private: Some or all of the data contained in this object is not appropriate for public dissemination. This object cannot be shared publicly in its current form."))
+                          contains = "Ready4Module"#,
+                          # slots = c(dissemination_1L_chr = "character"),
+                          # prototype = list(dissemination_1L_chr = "Private: Some or all of the data contained in this object is not appropriate for public dissemination. This object cannot be shared publicly in its current form.")
+                          )
