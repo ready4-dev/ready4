b674a1fa60419a751a3fb06a226d22903a38d8bf
Matthew Hamilton <matthew.hamilton@orygen.org.au>
2021-12-10 14:23:50 +1100

    added procureslot dmtn

--- R/mthd_procureSlot.R
+++ R/mthd_procureSlot.R
@@ -1,3 +1,14 @@
+#'
+#' procureSlot
+#' @name procureSlot-Ready4Module
+#' @description procureSlot method applied to Ready4Module
+#' @param x An object of class Ready4Module
+#' @param slot_nm_1L_chr Slot name (a length one character vector)
+#' @param ... Additional arguments
+#' @return NULL
+#' @rdname procureSlot-methods
+#' @aliases procureSlot,Ready4Module-method
+#' @export
 methods::setMethod("procureSlot", "Ready4Module", function(x,
                                                            slot_nm_1L_chr){
   y_xx <- eval(parse(text = paste0("x@",slot_nm_1L_chr)))
