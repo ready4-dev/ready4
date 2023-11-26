920e77f426df50cceaae5d67e823e17034d6393f
Matthew P Hamilton <30450935+matthewphamilton@users.noreply.github.com>
2023-11-26 20:16:25 +1100

    fix of example problem

--- R/fn_print.R
+++ R/fn_print.R
@@ -206,12 +206,7 @@ print_modules <- function (modules_tb, scroll_height_1L_chr = character(0), scro
 #' @importFrom kableExtra cell_spec kable kable_styling column_spec spec_image
 #' @examplesIf interactive()
 #'   libraries_tb <- get_libraries_tb(gh_repo_1L_chr = "ready4-dev/ready4")
-#'   # Print framework libraries (method 1)
-#'   update_libraries_tb(libraries_tb,
-#'                       url_stub_1L_chr = "https://ready4-dev.github.io/",
-#'                       include_1L_chr = "framework") %>%
-#'   print_packages()
-#'   # Print framework libraries (method 2)
+#'   # Print framework libraries
 #'   print_packages(gh_repo_1L_chr = "ready4-dev/ready4",
 #'                  include_1L_chr = "framework")
 #'   # Print module libraries (method 1)
--- data-raw/examples/print_packages.R
+++ data-raw/examples/print_packages.R
@@ -1,11 +1,6 @@
 if (interactive()) {
   libraries_tb <- get_libraries_tb(gh_repo_1L_chr = "ready4-dev/ready4")
-  # Print framework libraries (method 1)
-  update_libraries_tb(libraries_tb,
-                      url_stub_1L_chr = "https://ready4-dev.github.io/",
-                      include_1L_chr = "framework") %>%
-  print_packages()
-  # Print framework libraries (method 2)
+  # Print framework libraries
   print_packages(gh_repo_1L_chr = "ready4-dev/ready4",
                  include_1L_chr = "framework")
   # Print module libraries (method 1)
