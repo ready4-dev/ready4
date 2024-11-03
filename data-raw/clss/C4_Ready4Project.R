aceae52c3833c13a8fae18d9d8172f42431aa3d0
Matthew P Hamilton <30450935+matthewphamilton@users.noreply.github.com>
2024-11-04 07:18:58 +1100

    wip on prokect module

--- data-raw/clss/C4_Ready4Project.R
+++ data-raw/clss/C4_Ready4Project.R
@@ -12,53 +12,58 @@
 #' @exportClass Ready4Project
 Ready4Project <- setClass("Ready4Project",
                           contains = "Ready4Module",
-                          slots = c(additions_tb = "tbl_df",
-                                    brochure_repos_chr = character(0),
-                                    cls_extensions_tb = "tbl_df",
+                          slots = c(#additions_tb = "tbl_df",
+                                    badges_url_1L_chr = "character",
                                     dv_nm_1L_chr = "character",
                                     dv_root_1L_chr = "character",
+                                    dv_server_1L_chr = "character",
+                                    dv_toy_data_1L_chr = "character",
                                     dvs_tb = "tbl_df",
+                                    extensions_tb = "tbl_df",
                                     filter_cdns_ls = "list",
-                                    server_1L_chr = "character",
-                                    exclude_chr = "character",
-                                    framework_repos_chr = "character",
+                                    gh_org_1L_chr = "character",
                                     gh_repo_1L_chr = "character",
-                                    gh_tag_1L_chr = "character",
+                                    gh_repos_brochure_chr = "character",
+                                    gh_repos_exclude_chr = "character",
+                                    gh_repos_framework_chr ="character",
+                                    gh_repos_model_chr = "character",
+                                    gh_repos_programs_chr = "character",
+                                    gh_repos_subroutines_chr = "character",
+                                    gh_tag_1L_chr = "Documentation_0.0",
+                                    gh_url_stub_1L_chr = "character",
+                                    libraries_chr = "character",
                                     libraries_tb = "tbl_df",
                                     methods_tb = "tbl_df",
-                                    module_pkgs_chr = "character",
-                                    modules_tb = "tbl_df",
-                                    org_1L_chr = "character",
-                                    program_repos_chr = "character",
-                                    project_badges_url_1L_chr = "character",
-                                    repos_chr = "character",
+                                    modules_tb = "tbl_df",,
                                     sections_chr = "character",
-                                    subroutine_repos_chr = "character",
-                                    toy_data_dv_1L_chr = "character",
-                                    url_stub_1L_chr = "character",
+                                    url_for_prj_1L_chr = "character",
+                                    zenodo_1L_chr = "character",
                                     dissemination_1L_chr = "character"),
-                        prototype = list(additions_tb = make_additions_tb(),
-                                         brochure_repos_chr = character(0),
-                                         cls_extensions_tb = get_cls_extensions(get_libraries_tb())[0,],# get_cls_extensions(get_libraries_tb()) # NULL,
+                        prototype = list(#additions_tb = make_additions_tb(),
+                                         badges_url_1L_chr = character(0),#project_badges_url_1L_chr = "https://img.shields.io/badge/ready4"
                                          dv_nm_1L_chr = character(0),#"ready4",
                                          dv_root_1L_chr = character(0),#"https://dataverse.harvard.edu/dataverse/",# root_1L_chr - print_data
+                                         dv_server_1L_chr = "dataverse.harvard.edu", #server_1L_chr
+                                         dv_toy_data_1L_chr = "fakes", #toy_data_dv_1L_chr =
                                          dvs_tb = ready4::get_datasets_tb()[0,],#NULL,
+                                         extensions_tb = get_cls_extensions(get_libraries_tb())[0,],# cls_extensions_tb = get_cls_extensions(get_libraries_tb()) # NULL,
                                          filter_cdns_ls = list(),#NULL,
-                                         server_1L_chr = "dataverse.harvard.edu",
-                                         exclude_chr = character(0),
-                                         framework_repos_chr = character(0),
+                                         gh_org_1L_chr = character(0), # org_1L_chr = "ready4-dev",
                                          gh_repo_1L_chr = character(0),#"ready4-dev/ready4",
+                                         gh_repos_brochure_chr = character(0), # brochure_repos_chr =
+                                         gh_repos_exclude_chr = character(0), #exclude_chr =
+                                         gh_repos_framework_chr = character(0), # framework_repos_chr
+                                         gh_repos_model_chr = character(0), # model_repos_chr
+                                         gh_repos_programs_chr = character(0), # program_repos_chr
+                                         gh_repos_subroutines_chr = character(0), # subroutine_repos_chr
                                          gh_tag_1L_chr = "Documentation_0.0",
-                                         libraries_tb = make_libraries_tb(), #pkg_extensions_tb = NULL
+                                         gh_url_stub_1L_chr = character(0), #url_stub_1L_chr = "https://ready4-dev.github.io/"
+                                         libraries_chr = character(0), # module_pkgs_chr = make_modules_pkgs_chr() model_repos_chr = character(0),
+                                         libraries_tb = make_libraries_tb(), #get_libraries_tb() # pkg_extensions_tb = NULL
                                          #methods_chr = NULL, # print_methods - Can be derived from included Ready4Module
                                          methods_tb = get_methods_tb(), #NULL, # print_methods
-                                         module_pkgs_chr = character(0), # make_modules_pkgs_chr() model_repos_chr = character(0),
-                                         modules_tb = ready4::get_modules_tb()[0,], #### print_modules
-                                         org_1L_chr = character(0), # "ready4-dev",
-                                         program_repos_chr = character(0),
-                                         project_badges_url_1L_chr = character(0),#"https://img.shields.io/badge/ready4"
-                                         repos_chr = character(0), #??
+                                         modules_tb = get_modules_tb()[0,], #### print_modules
                                          sections_chr = character(0),
-                                         subroutine_repos_chr = character(0),
-                                         toy_data_dv_1L_chr = "fakes",
-                                         url_stub_1L_chr = character(0))) #"https://ready4-dev.github.io/"
+                                         url_for_prj_1L_chr = character(0),
+                                         zenodo_1L_chr = "ready4"
+                                         ))
